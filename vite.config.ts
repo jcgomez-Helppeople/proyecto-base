57c15ffefee9a5d270421e01b3267d3ca23a5ec6
jcgomez-Helppeople <jcgomez@helppeoplecloud.com>
2025-03-12 11:17:57 -0500

    Se agrega las declaraciones con vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), dts(), tailwindcss()],
+  plugins: [
+    react(),
+    dts({
+      outDir: "dist",
+      insertTypesEntry: true, // Esto inserta una entrada de types en package.json automáticamente
+    }),
+    tailwindcss(),
+  ],
   build: {
     lib: {
-      entry: 'src/index.ts', // Punto de entrada de tu librería
-      name: 'MyLibrary',
-      fileName: (format) => `my-library.${format}.js`
+      entry: "src/index.ts", // Punto de entrada de tu librería
+      name: "MyLibrary",
+      fileName: (format) => `my-library.${format}.js`,
     },
     rollupOptions: {
       // Excluir dependencias externas para evitar duplicados en los microfrontends
-      external: ['react', 'react-dom', 'primereact']
-    }
-  }
+      external: ["react", "react-dom", "primereact"],
+    },
+  },
 });
