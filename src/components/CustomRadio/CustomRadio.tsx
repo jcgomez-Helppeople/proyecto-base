561c8a0200ed758b3eba2756da30d1f052657cea
jcgomez-Helppeople <jcgomez@helppeoplecloud.com>
2025-05-06 12:22:09 -0500

    Se divide el Radio y el RadioGroup

--- src/components/CustomRadio/CustomRadio.tsx
+++ src/components/CustomRadio/CustomRadio.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Radio as AntRadio, RadioProps as AntRadioProps, RadioGroupProps } from "antd";
+import { Radio as AntRadio, RadioProps as AntRadioProps } from "antd";
 
 export interface CustomRadioProps extends AntRadioProps {
   /**
@@ -14,12 +14,6 @@ const CustomRadio: React.FC<CustomRadioProps> = ({ label, ...rest }) => {
       {label}
     </AntRadio>
   );
-};
-
-export interface CustomRadioGroupProps extends RadioGroupProps {}
-
-export const CustomRadioGroup: React.FC<CustomRadioGroupProps> = (props) => {
-  return <AntRadio.Group {...props} />;
 };
 
 export default CustomRadio;
