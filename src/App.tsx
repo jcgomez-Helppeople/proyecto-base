c6b0f897d38d74757bdedceea27fc1cdea6dec9c
jcgomez-Helppeople <jcgomez@helppeoplecloud.com>
2025-05-19 16:30:48 -0500

    Cambios de prueba

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Layout from "./examples/Layout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PoliciesScreen from "./examples/PoliciesScreen";
-// import CreatePolicyScreen from "./examples/CreatePolicyScreen";
+import CreatePolicyScreen from "./examples/CreatePolicyScreen";
 // import RFCCreate from "./examples/CreateChange";
 import { ConfigProvider } from "antd";
 
@@ -21,7 +21,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<PoliciesScreen />} />
-          {/* <Route path="/create-policy" element={<CreatePolicyScreen />} /> */}
+          <Route path="/create-policy" element={<CreatePolicyScreen />} />
         </Routes>
       </Router>
     </Layout>
