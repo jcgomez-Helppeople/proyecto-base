eb7ea7c3bccdff5788aa45739a0f5171e40a06f0
jcgomez-Helppeople <jcgomez@helppeoplecloud.com>
2025-05-19 19:12:35 -0500

    Se quita las advertencias para poder compilar.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PoliciesScreen from "./examples/PoliciesScreen";
 import CreatePolicyScreen from "./examples/CreatePolicyScreen";
 // import RFCCreate from "./examples/CreateChange";
-import { ConfigProvider } from "antd";
 import LibraryThemeProvider  from "./theme/LibraryThemeProvider";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
